3a7db6c1fe5bf80d26530047895be9c01ab26f17
jesahquiado <jesahdaquiado@gmail.com>
2025-10-30 00:42:49 -0700

    updated library

--- src/main/Main.java
+++ src/main/Main.java
@@ -91,7 +91,7 @@ public class Main {
                                             break;
                                     }
                                 } else if (type.equals("Librian")) {
-                                    System.out.println("WELCOME TO TEACHER DASHBOARD");
+                                    System.out.println("WELCOME TO LIBRIAN DASHBOARD");
                                     librianMenu();
                                 }
                             }
@@ -158,65 +158,130 @@ public class Main {
     }
 
     public static void librianMenu() {
-        Scanner sc = new Scanner(System.in);
-        config conf = new config();
-        int choice;
+    Scanner sc = new Scanner(System.in);
+    config conf = new config();
+    int choice;
 
-        do {
-            System.out.println("==== MAIN MENU ====");
-            System.out.println("1. Add User: ");
-            System.out.println("2. Add Books: ");
-            System.out.println("3. Add Borrowed Book: ");
-            System.out.println("5. Exit ");
-            System.out.print("Enter Choice: ");
-            choice = sc.nextInt();
+    do {
+        System.out.println("==== MAIN MENU ====");
+        System.out.println("1. Add User: ");
+        System.out.println("2. Add Books: ");
+        System.out.println("3. Add Borrowed Book: ");
+        System.out.println("4. Add Customer (Reader): "); // New option
+        System.out.println("5. Exit ");
+        System.out.print("Enter Choice: ");
+        choice = sc.nextInt();
 
-            switch (choice) {
-                case 1:
-                    int userchoice;
-                    System.out.println("==== USER MENU ====");
-                    System.out.println("1. Add User");
-                    System.out.println("2. View User");
-                    System.out.println("3. Update User");
-                    System.out.println("4. Delete User");
+        switch (choice) {
+            case 1:
+                int userchoice;
+                System.out.println("==== USER MENU ====");
+                System.out.println("1. Add User");
+                System.out.println("2. View User");
+                System.out.println("3. Update User");
+                System.out.println("4. Delete User");
+                System.out.println("5. Back");
+                userchoice = sc.nextInt();
+
+                switch (userchoice) {
+                    case 1:
+                        System.out.println("Enter Name: ");
+                        String name = sc.next();
+                        System.out.println("Enter Email: ");
+                        String em = sc.next();
+
+                        String sql = "INSERT INTO tbl_user (u_name, u_email) VALUES (?, ?)";
+                        conf.addRecord(sql, name, em);
+                        break;
+
+                    case 2:
+                        viewUsers();
+                        break;
+
+                    case 3:
+                        viewUsers();
+                        System.out.println("Enter id to update: ");
+                        int id = sc.nextInt();
+                        System.out.println("Enter new Name: ");
+                        name = sc.next();
+                        System.out.println("Enter new Email: ");
+                        em = sc.next();
+                        String qry = "UPDATE tbl_user SET u_name = ?, u_contact = ?, u_email = ? WHERE u_id = ?";
+                        conf.updateRecord(qry, name, em, id);
+                        viewUsers();
+                        break;
+
+                    case 4:
+                        viewUsers();
+                        System.out.println("Enter id to delete: ");
+                        id = sc.nextInt();
+                        qry = "DELETE FROM tbl_user WHERE u_id = ?";
+                        conf.deleteRecord(qry, id);
+                        viewUsers();
+                        break;
+
+                    case 5:
+                        System.out.println("Going back to main menu...");
+                        break;
+
+                    default:
+                        System.out.println("Invalid Choice!");
+                }
+                break;
+
+            case 2:
+                int bookChoice;
+                do {
+                    System.out.println("==== BOOK MENU ====");
+                    System.out.println("1. Add Book");
+                    System.out.println("2. View Books");
+                    System.out.println("3. Update Book");
+                    System.out.println("4. Delete Book");
                     System.out.println("5. Back");
-                    userchoice = sc.nextInt();
+                    System.out.print("Enter choice: ");
+                    bookChoice = sc.nextInt();
 
-                    switch (userchoice) {
+                    switch (bookChoice) {
                         case 1:
-                            System.out.println("Enter Name: ");
-                            String name = sc.next();
-                            System.out.println("Enter Email: ");
-                            String em = sc.next();
-
-                            String sql = "INSERT INTO tbl_user (u_name, u_email) VALUES (?, ?)";
-                            conf.addRecord(sql, name, em);
+                            System.out.print("Enter Book Title: ");
+                            String title = sc.next();
+                            System.out.println("Enter Author: ");
+                            String author = sc.next();
+                            
+                            String addBookSQL = "INSERT INTO tbl_books (bok_title, bok_author) VALUES (?, ?)";
+                            conf.addRecord(addBookSQL, title, author);
+                            System.out.println("Book added successfully!");
                             break;
 
                         case 2:
-                            viewUsers();
+                            viewbooks();
                             break;
 
                         case 3:
-                            viewUsers();
-                            System.out.println("Enter id to update: ");
-                            int id = sc.nextInt();
-                            System.out.println("Enter new Name: ");
-                            name = sc.next();
-                            System.out.println("Enter new Email: ");
-                            em = sc.next();
-                            String qry = "UPDATE tbl_user SET u_name = ?, u_contact = ?, u_email = ? WHERE u_id = ?";
-                            conf.updateRecord(qry, name, em, id);
-                            viewUsers();
+                            viewbooks();
+                            
+                            System.out.print("Enter Book ID to Update: ");
+                            int bid = sc.nextInt();
+                            System.out.print("Enter New Title: ");
+                            title = sc.next();
+                            System.out.print("Enter New Author: ");
+                            author = sc.next();
+                            
+                            String updateBookSQL = "UPDATE tbl_books SET bok_title = ?, bok_author = ? WHERE bok_id = ?";
+                            conf.updateRecord(updateBookSQL, title, author, bid);
+                            System.out.println("Book updated successfully!");
+                            viewbooks();
                             break;
 
                         case 4:
-                            viewUsers();
-                            System.out.println("Enter id to delete: ");
-                            id = sc.nextInt();
-                            qry = "DELETE FROM tbl_user WHERE u_id = ?";
-                            conf.deleteRecord(qry, id);
-                            viewUsers();
+                            viewbooks();
+
+                            System.out.print("Enter Book ID to Delete: ");
+                            int delid = sc.nextInt();
+                            String deleteBookSQL = "DELETE FROM tbl_books WHERE bok_id = ?";
+                            conf.deleteRecord(deleteBookSQL, delid);
+                            System.out.println("Book deleted successfully!");
+                            viewbooks();
                             break;
 
                         case 5:
@@ -224,138 +289,108 @@ public class Main {
                             break;
 
                         default:
-                            System.out.println("Invalid Choice!");
+                            System.out.println("Invalid choice!");
                     }
-                    break;
-
-                case 2:
-                    int bookChoice;
-                    do {
-                        System.out.println("==== BOOK MENU ====");
-                        System.out.println("1. Add Book");
-                        System.out.println("2. View Books");
-                        System.out.println("3. Update Book");
-                        System.out.println("4. Delete Book");
-                        System.out.println("5. Back");
-                        System.out.print("Enter choice: ");
-                        bookChoice = sc.nextInt();
-
-                        switch (bookChoice) {
-                            case 1:
-                                System.out.print("Enter Book Title: ");
-                                String title = sc.next();
-                                System.out.println("Enter Author: ");
-                                String author = sc.next();
-                                
-                                String addBookSQL = "INSERT INTO tbl_books (bok_title, bok_author) VALUES (?, ?)";
-                                conf.addRecord(addBookSQL, title, author);
-                                System.out.println("Book added successfully!");
-                                break;
-
-                            case 2:
-                                viewbooks();
-                                break;
-
-                            case 3:
-                               viewbooks();
-                                
-                                System.out.print("Enter Book ID to Update: ");
-                                int bid = sc.nextInt();
-                                System.out.print("Enter New Title: ");
-                                title = sc.next();
-                                System.out.print("Enter New Author: ");
-                                author = sc.next();
-                                
-                                String updateBookSQL = "UPDATE tbl_books SET bok_title = ?, bok_author = ? WHERE bok_id = ?";
-                                conf.updateRecord(updateBookSQL, title, author, bid);
-                                System.out.println("Book updated successfully!");
-                                viewbooks();
-                                break;
-
-                            case 4:
-                                viewbooks();
-
-                                System.out.print("Enter Book ID to Delete: ");
-                                int delid = sc.nextInt();
-                                String deleteBookSQL = "DELETE FROM tbl_books WHERE bok_id = ?";
-                                conf.deleteRecord(deleteBookSQL, delid);
-                                System.out.println("Book deleted successfully!");
-                                viewbooks();
-                                break;
-
-                            case 5:
-                                System.out.println("Going back to main menu...");
-                                break;
-
-                            default:
-                                System.out.println("Invalid choice!");
-                        }
-                    } while (bookChoice != 5);
-                    break;
-
-                case 3:
-    int borrowChoice;
-    do {
-        System.out.println("==== BORROWED BOOK MENU ====");
-        System.out.println("1. Borrow a Book");
-        System.out.println("2. View Borrowed Books");
-        System.out.println("3. Return a Book");
-        System.out.println("4. Back");
-        System.out.print("Enter choice: ");
-        borrowChoice = sc.nextInt();
-
-        switch (borrowChoice) {
-            case 1:
-                System.out.println("Select User:");
-                viewUsers();
-                System.out.print("Enter User ID: ");
-                int uid = sc.nextInt();
-
-                System.out.println("Select Book:");
-                viewbooks();
-                System.out.print("Enter Book ID: ");
-                int bid = sc.nextInt();
-
-                System.out.print("Enter Borrow Date: ");
-                int bdate = sc.nextInt();
-                System.out.print("Enter Return Date: ");
-                int rdate = sc.nextInt();
-
-                String addBorrowSQL = "INSERT INTO tbl_borrow (bok_id, u_id, b_date, b_return) VALUES (?, ?, ?, ?)";
-                conf.addRecord(addBorrowSQL, bid, uid, bdate, rdate);
-                System.out.println("Book borrowed successfully!");
-                break;
-
-            case 2:
-                viewborrow();
+                } while (bookChoice != 5);
                 break;
 
             case 3:
-                viewborrow();
+                int borrowChoice;
+                do {
+                    System.out.println("==== BORROWED BOOK MENU ====");
+                    System.out.println("1. Borrow a Book");
+                    System.out.println("2. View Borrowed Books");
+                    System.out.println("3. Return a Book");
+                    System.out.println("4. Back");
+                    System.out.print("Enter choice: ");
+                    borrowChoice = sc.nextInt();
 
-                System.out.print("Enter Borrow ID to delete (return): ");
-                int brid = sc.nextInt();
-                String deleteBorrowSQL = "DELETE FROM tbl_borrow WHERE b_id = ?";
-                conf.deleteRecord(deleteBorrowSQL, brid);
-                System.out.println("Book marked as returned (record deleted)!");
-                viewborrow();
+                    switch (borrowChoice) {
+                        case 1:
+                            System.out.println("Select User:");
+                            viewUsers();
+                            System.out.print("Enter User ID: ");
+                            int uid = sc.nextInt();
+
+                            System.out.println("Select Book:");
+                            viewbooks();
+                            System.out.print("Enter Book ID: ");
+                            int bid = sc.nextInt();
+                            
+
+                            System.out.print("Enter Borrow Date: ");
+                            String bdate = sc.next();
+                            System.out.print("Enter Return Date: ");
+                            String rdate = sc.next();
+                            
+                            if (bdate == rdate) {
+                                System.out.println("Error: Borrow date and return date cannot be the same!");
+                                break; // stop this case and go back to the menu
+                            }
+
+                            String addBorrowSQL = "INSERT INTO tbl_borrow (bok_id, u_id, b_date, b_return) VALUES (?, ?, ?, ?)";
+                            conf.addRecord(addBorrowSQL, bid, uid, bdate, rdate);
+                            System.out.println("Book borrowed successfully!");
+                            break;
+
+                        case 2:
+                            viewborrow();
+                            break;
+
+                        case 3:
+                            viewborrow();
+
+                            System.out.print("Enter Borrow ID to delete (return): ");
+                            int brid = sc.nextInt();
+                            String deleteBorrowSQL = "DELETE FROM tbl_borrow WHERE b_id = ?";
+                            conf.deleteRecord(deleteBorrowSQL, brid);
+                            System.out.println("Book marked as returned (record deleted)!");
+                            viewborrow();
+                            break;
+
+                        case 4:
+                            System.out.println("Going back to main menu...");
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                } while (borrowChoice != 4);
                 break;
 
-            case 4:
-                System.out.println("Going back to main menu...");
+            case 4: // New case for adding a reader/customer
+                System.out.println("==== ADD READER (CUSTOMER) ====");
+                System.out.println("Enter Reader Name: ");
+                String readerName = sc.next();
+                System.out.println("Enter Reader Email: ");
+                String readerEmail = sc.next();
+
+                // Ensure the email is unique
+                while (true) {
+                    String qry = "SELECT * FROM tbl_user WHERE u_email = ?";
+                    java.util.List<java.util.Map<String, Object>> result = conf.fetchRecords(qry, readerEmail);
+
+                    if (result.isEmpty()) {
+                        break;
+                    } else {
+                        System.out.println("Email Already Exists! Enter another email: ");
+                        readerEmail = sc.next();
+                    }
+                }
+
+                String readerSql = "INSERT INTO tbl_user(u_name, u_email, u_type, u_status, u_pass) VALUES (?, ?, ?, ?, ?)";
+                conf.addRecord(readerSql, readerName, readerEmail, "Customer", "Customer", "defaultPassword");
+                System.out.println("Reader (Customer) added successfully!");
+                break;
+
+            case 5:
+                System.out.println("Exiting...");
                 break;
 
             default:
                 System.out.println("Invalid choice!");
         }
-                    } while (borrowChoice != 4);
-                break;
-
-                default:
-                    System.out.println("Invalid choice!");
-                    break;
-            }
-        } while (choice != 5);
-    }
+    } while (choice != 5);
+}
 }
 
